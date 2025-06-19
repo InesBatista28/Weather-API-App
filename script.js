18a04102c76ee676b0fff4bc2a2e98ad0d70e957
InesBatista74 <simoesbatista1991@gmail.com>
2025-06-19 01:41:41 +0100

    weatherData

---
+++ script.js
@@ -0,0 +1,58 @@
+const cityInput = document.querySelector('.city-input')
+const searchBtn = document.querySelector('.search-btn')
+
+const weatherInfoSection = document.querySelector('.weather-info')
+const notFoundSection = document.querySelector('.not-found')
+const searchCitySection = document.querySelector('.search-city')
+
+const apiKey = '4a7a904749bf2a2ad5c9f60ae4aff396'
+
+searchBtn.addEventListener('click', () => {
+    if (cityInput.value.trim() != '') {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+})
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter' &&
+        cityInput.value.trim() != ''
+    ) {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+})
+
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
+
+    const response = await fetch(apiUrl)
+    return response.json()
+}
+
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData('weather', city)
+
+    if (weatherData.cod != 200) {
+        showDisplaySection(notFoundSection)
+        return
+    } 
+    console.log(weatherData)
+
+    const {
+        name: country, 
+        main: { temp, humidity }, 
+        weather: [{ id, main }]
+        wind: { speed }
+    } = weatherData
+
+    showDisplaySection(weatherInfoSection)
+}
+
+function showDisplaySection(section) {
+    [weatherInfoSection, searchCitySection, notFoundSection]
+        .forEach(section => section.style.display = 'none') 
+    
+    section.style.display = 'flex'
+}
